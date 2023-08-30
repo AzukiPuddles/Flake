accf034f34b8bddc84dadc290a9a331814b06f5f
AzukiPuddles <Azuki@Pudl.es>
2023-08-30 11:15:25 -0400

    gfdh

--- flake.nix
+++ flake.nix
@@ -14,13 +14,6 @@
     selfPkgs = import ./pkgs;
   in {
     nixosConfigurations = import ./system/core.nix {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/nixos/configuration.nix
-        ];
-        specialArgs = { inherit inputs; };
-      };
       inherit self nixpkgs inputs;
     };
   };
--- system/core.nix
+++ system/core.nix
@@ -8,7 +8,7 @@
     specialArgs = {inherit self inputs;};
     system = "x86_64-linux";
     modules = [
-      /etc/nixos/hardware-configuration.nix
+      ./hosts/nixos/configuration.nix
       ./bootloader.nix
       ./desktop.nix
       ./network.nix
